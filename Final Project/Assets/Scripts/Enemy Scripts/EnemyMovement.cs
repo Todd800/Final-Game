002ea1a9a81b7d1500f15bcadc89116817f5c51a
Todd800 <Chatod_Henderson1@bloomfield.edu>
2022-05-08 02:06:51 -0400

    Attack CheckPoint

--- Final Project/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ Final Project/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -11,23 +11,22 @@ public class EnemyMovement : MonoBehaviour
 
     private Transform playerTarget;
 
+
     public float attack_Distance = 1f;
     public float chase_Player_After_Attack = 1f;
-    
+
     private float current_Attack_Time;
     private float default_Attack_Time = 2f;
 
-    private bool followPlayer;
-    private bool attackPlayer;
+    private bool followPlayer, attackPlayer;
 
-    void Awake() 
+    void Awake()
     {
         enemyAnim = GetComponentInChildren<CharacterAnimation>();
         myBody = GetComponent<Rigidbody>();
 
         playerTarget = GameObject.FindWithTag(Tags.PLAYER_TAG).transform;
     }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -41,27 +40,28 @@ public class EnemyMovement : MonoBehaviour
         Attack();
     }
 
-    void FixedUpate() 
+    void FixedUpdate()
     {
         FollowTarget();
     }
 
-    void FollowTarget() 
+    void FollowTarget()
     {
         if (!followPlayer) return;
 
-        if(Vector3.Distance(transform.position, playerTarget.position) > attack_Distance) 
+        if (Vector3.Distance(transform.position, playerTarget.position) > attack_Distance)
         {
             transform.LookAt(playerTarget);
             myBody.velocity = transform.forward * speed;
 
-            if(myBody.velocity.sqrMagnitude != 0) 
+            if (myBody.velocity.sqrMagnitude != 0)
             {
                 enemyAnim.Walk(true);
-            } 
+            }
+
         }
 
-        else if (Vector3.Distance(transform.position, playerTarget.position) <= attack_Distance) 
+        else if (Vector3.Distance(transform.position, playerTarget.position) <= attack_Distance)
         {
             myBody.velocity = Vector3.zero;
             enemyAnim.Walk(false);
@@ -70,26 +70,28 @@ public class EnemyMovement : MonoBehaviour
             attackPlayer = true;
         }
 
-    } // follow target
+    }
 
-    void Attack() 
+    void Attack()
     {
         if (!attackPlayer) return;
 
         current_Attack_Time += Time.deltaTime;
 
-        if(current_Attack_Time > default_Attack_Time) 
+        if (current_Attack_Time > default_Attack_Time)
         {
             enemyAnim.EnemyAttack(Random.Range(0, 3));
 
             current_Attack_Time = 0f;
+
         }
 
-        if(Vector3.Distance(transform.position, playerTarget.position) > attack_Distance + chase_Player_After_Attack) 
+        if (Vector3.Distance(transform.position, playerTarget.position) > attack_Distance + chase_Player_After_Attack)
         {
             attackPlayer = false;
             followPlayer = true;
         }
+
     }
 
-}// class
+}
